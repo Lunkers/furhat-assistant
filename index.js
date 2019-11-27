caf784eacf65f413e8a90cfc983865228dd6c85f
Fredrik Lundkvist <flundkvi@kth.se>
2019-11-27 10:58:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-import Furhat from 'furhat-core';
+//import Furhat from 'furhat-core';
+const GoogleAssistant = require('./googleassistant')
+const deviceCredentials = require('./devicecredentials.json')
 
+//setup assistant credentials
+const CREDENTIALS = {
+    client_id: deviceCredentials.client_id,
+    client_secret: deviceCredentials.client_secret,
+    refresh_token: deviceCredentials.refresh_token,
+    type: "authorized_user"
+};
+//create google assistant object
+const assistant = new GoogleAssistant(CREDENTIALS);
+
+//test assistant connection
+assistant.assist("what time is it?").then(({text})=> console.log(text)).catch(reason => console.log(reason));
+
+/*
 let furhat = new Furhat()
 
 furhat.init('localhost', '8080', 'api', (status, hat) => {
@@ -7,5 +23,5 @@ furhat.init('localhost', '8080', 'api', (status, hat) => {
         hat.subscribe("furhatos.event.senses.senseSpeech", event => console.log(event))
         hat.say('I am connected!')
     }
-   
-})
+
+})*/
